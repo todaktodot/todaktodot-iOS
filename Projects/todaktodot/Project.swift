a1068b721cb07244f2a5f9ea6714ce4696f37736
DAEJINLIM <daejin.limm@gmail.com>
2026-01-28 19:32:52 +0900

    Feat: 번들아이디 xcconfig 분리 적용

--- Projects/todaktodot/Project.swift
+++ Projects/todaktodot/Project.swift
@@ -22,7 +22,7 @@ let project = Project(
             name: "todaktodot",
             destinations: .iOS,
             product: .app,
-            bundleId: "com.todaktodot.dev",
+            bundleId: "$(PRODUCT_BUNDLE_IDENTIFIER)",
             deploymentTargets: .iOS("15.0"),
             infoPlist: .extendingDefault(
                 with: [
