7c77adecd50ac02e3bd9f27ce0f82d3f5a6f762d
DAEJINLIM <daejin.limm@gmail.com>
2026-03-10 00:08:03 +0900

    Update Project.swift

--- Projects/todaktodot/Project.swift
+++ Projects/todaktodot/Project.swift
@@ -105,7 +105,10 @@ let project = Project(
                     "DEVELOPMENT_TEAM": "5HY2NNF4HY",
                     "VERSIONING_SYSTEM": "apple-generic",
                     "CURRENT_PROJECT_VERSION": "1",
-                    "MARKETING_VERSION": "1.0.0"
+                    "MARKETING_VERSION": "1.0.0",
+                    "CODE_SIGN_STYLE": "Manual",
+                    "CODE_SIGN_IDENTITY": "Apple Distribution",
+                    "PROVISIONING_PROFILE_SPECIFIER": "match AppStore info.todaktodot",
                 ],
                 configurations: [
                     .debug(name: "Debug", xcconfig: "Config/Debug.xcconfig"),
