7a8a8b440e2553871da12968e82b69c8e83a5fb6
Da-hye0 <ekgP90806@gmail.com>
2026-03-10 13:22:06 +0900

    Fix: 메인카드 데이터 수정

--- Projects/todaktodot/Sources/Data/Repositories/CardRepositoryImpl.swift
+++ Projects/todaktodot/Sources/Data/Repositories/CardRepositoryImpl.swift
@@ -118,7 +118,8 @@ final class CardRepositoryImpl: CardRepository {
         return networkManager.requestOptional(with: endpoint)
             .map { _ in Result<Void, Error>.success(()) }
             .catch { error in .just(.failure(error)) }
-
+    }
+    
     func notiAgree() -> Observable<Bool> {
         var parameters: [String: String] = [ "infoAlarmYN" : "Y" ]
         
