2cfe37307f000cb8e0c73338efddb6e182112894
DAEJINLIM <daejin.limm@gmail.com>
2026-04-27 02:01:34 +0900

    Feat: 닉네임 정책 수정, 설명 라벨 추가, 중복 제거

--- Projects/todaktodot/Sources/Presentation/Signin/View/NicknameViewController.swift
+++ Projects/todaktodot/Sources/Presentation/Signin/View/NicknameViewController.swift
@@ -39,7 +39,7 @@ final class NicknameViewController: UIViewController, View {
     }
     
     private let textFiled = UITextField().then {
-        $0.placeholder = "닉네임을 입력해주세요"
+        $0.placeholder = "글자 수는 1~10자까지 입력 가능해요"
         $0.font = .pretenMedium(16)
         $0.textColor = .grayScale900
         $0.backgroundColor = .white
@@ -49,6 +49,12 @@ final class NicknameViewController: UIViewController, View {
         $0.layer.cornerRadius = 6
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor.grayScale200.cgColor
+    }
+    
+    private let textFiledDescriptionLabel = TDLabel().then {
+        $0.text = "한글, 영어, 숫자, 이모지 모두 사용 가능해요!"
+        $0.font = .pretenMedium(12)
+        $0.textColor = .grayScale600
     }
     
     private let nextButton = UIButton(type: .system).then {
@@ -130,6 +136,10 @@ final class NicknameViewController: UIViewController, View {
             $0.addItem(textFiled)
                 .marginTop(40)
                 .height(56)
+            
+            $0.addItem(textFiledDescriptionLabel)
+                .marginTop(8)
+                .marginLeft(8)
         }
     }
     
@@ -175,8 +185,9 @@ final class NicknameViewController: UIViewController, View {
             .disposed(by: disposeBag)
         
         nextButton.rx.tap
-            .compactMap { self.textFiled.text }
+            .withLatestFrom(textFiled.rx.text.orEmpty)
             .filter { !$0.isEmpty }
+            .distinctUntilChanged()
             .map { CoupleReactor.Action.tapNicknameButton($0) }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
@@ -219,7 +230,7 @@ extension NicknameViewController: UITextFieldDelegate {
     ) -> Bool {
         if string.isEmpty { return true }
         
-        let allowedPattern = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z]+$"
+        let allowedPattern = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\\p{Emoji}]+$"
         let isValidInput = string.range(of: allowedPattern, options: .regularExpression) != nil
         
         if !isValidInput { return false }
